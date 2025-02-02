2163993dfca6dcddbc146b7b55ec017225cc8f8a
James Koh <kohjianjie@gmail.com>
2025-02-03 01:29:47 +0800

    added a list to keep track of tasks
    added MAX_TASKS constant
    UI for adding tasks and listing tasks

--- src/main/java/Ava.java
+++ src/main/java/Ava.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 public class Ava {
 
     public static final String LINE_SEPARATOR = "_____________________________";
+    public static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-
+        String[] list = new String[MAX_TASKS];
+        int counter = 0;
         greetings();
         do {
             line = in.nextLine();
-            System.out.println(LINE_SEPARATOR);
-            System.out.println(line);
-            System.out.println(LINE_SEPARATOR);
+            if (!line.equals("list")) {
+                list[counter] = line;
+                counter++;
+                System.out.println(LINE_SEPARATOR);
+                System.out.println("added: " + line);
+                System.out.println(LINE_SEPARATOR);
+            } else {
+                System.out.println(LINE_SEPARATOR);
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ": " + list[i]);
+                }
+                System.out.println(LINE_SEPARATOR);
+            }
         } while (!line.equals("bye"));
         goodbyes();
     }
