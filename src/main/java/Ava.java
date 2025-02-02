64bcda74c45cd587bc0751ab809a66dfe3e43c7e
James Koh <kohjianjie@gmail.com>
2025-02-03 00:48:14 +0800

    Introduced LINE_SEPARATOR constant

--- src/main/java/Ava.java
+++ src/main/java/Ava.java
@@ -1,10 +1,13 @@
 public class Ava {
+
+    public static final String LINE_SEPARATOR = "_____________________________";
+
     public static void main(String[] args) {
-        System.out.println("_____________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Hello! I'm Ava");
         System.out.println("What can I do for you?");
-        System.out.println("_____________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 }
