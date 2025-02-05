f3f04df0f7bbf4482c723327a385ed61087f3d58
James Koh <kohjianjie@gmail.com>
2025-02-06 03:01:55 +0800

    Created Todo, Deadline, and Event child classes of Task
    Applied SLAP principles
    replaced if-else statements with switch statements
    implemented mark and unmark to be able to read task description and mark accordingly

--- src/main/java/Ava.java
+++ src/main/java/Ava.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.List;
 
 public class Ava {
 
@@ -12,35 +11,121 @@ public class Ava {
         Task[] list = new Task[MAX_TASKS];
         int counter = 0;
         greetings();
+
         do {
             line = in.nextLine();
-            if (line.equals("list")) {
-                System.out.println(LINE_SEPARATOR);
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i + 1) + ": " + list[i]);
-                }
-                System.out.println(LINE_SEPARATOR);
-            } else if (line.startsWith("mark")) {
-                int index = Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1;
-                list[index].setDone();
-                System.out.println(LINE_SEPARATOR);
-                System.out.println("Nice! I've marked this task as done:\n  " + list[index]);
-                System.out.println(LINE_SEPARATOR);
-            } else if (line.startsWith("unmark")) {
-                int index = Integer.parseInt(line.substring(line.indexOf(" ") + 1)) - 1;
-                list[index].setNotDone();
-                System.out.println(LINE_SEPARATOR);
-                System.out.println("OK, I've marked this task as not done yet:\n  " + list[index]);
-                System.out.println(LINE_SEPARATOR);
-            } else {
-                list[counter] = new Task(line);
+            switch (line.split(" ")[0]) {
+            case "list":
+                printTaskList(list, counter);
+                break;
+
+            case "mark":
+                handleMark(line, list, counter);
+                break;
+
+            case "unmark":
+                handleUnmark(line, list, counter);
+                break;
+
+            case "bye":
+                // Exit condition: just exit the loop
+                break;
+
+            case "todo":
+                addTask(new Todo(line.substring(5)), list, counter);
                 counter++;
-                System.out.println(LINE_SEPARATOR);
-                System.out.println("added: " + line);
-                System.out.println(LINE_SEPARATOR);
+                break;
+
+            case "deadline":
+                int byIndex = line.indexOf("/by");
+                String deadline = line.substring(9, byIndex - 1);
+                String by = line.substring(byIndex + 4);
+                addTask(new Deadline(deadline, by), list, counter);
+                counter++;
+                break;
+
+            case "event":
+                int fromIndex = line.indexOf("/from");
+                String event = line.substring(6, fromIndex - 1);
+                int toIndex = line.indexOf("/to");
+                String from = line.substring(fromIndex + 6, toIndex - 1);
+                String to = line.substring(toIndex + 4);
+                addTask(new Event(event, from, to), list, counter);
+                counter++;
+                break;
+
+            default:
+                addTask(new Task(line), list, counter);
+                counter++;
+                break;
             }
         } while (!line.equals("bye"));
+
         goodbyes();
+    }
+
+    private static void printTaskList(Task[] list, int counter) {
+        System.out.println(LINE_SEPARATOR);
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + ": " + list[i].toString());
+        }
+        System.out.println(LINE_SEPARATOR);
+    }
+
+    private static void handleMark(String line, Task[] list, int counter) {
+        String toMark = line.substring(line.indexOf(" ") + 1);
+        int index = getTaskIndex(toMark, list, counter);
+
+        if (index == -1) {
+            System.out.println(LINE_SEPARATOR);
+            System.out.println(toMark + " not in list!");
+            System.out.println(LINE_SEPARATOR);
+        } else {
+            list[index].setDone();
+            System.out.println(LINE_SEPARATOR);
+            System.out.println("Nice! I've marked this task as done:\n  " + list[index]);
+            System.out.println(LINE_SEPARATOR);
+        }
+    }
+
+    private static void handleUnmark(String line, Task[] list, int counter) {
+        String toMark = line.substring(line.indexOf(" ") + 1);
+        int index = getTaskIndex(toMark, list, counter);
+
+        if (index == -1) {
+            System.out.println(LINE_SEPARATOR);
+            System.out.println(toMark + " not in list!");
+            System.out.println(LINE_SEPARATOR);
+        } else {
+            list[index].setNotDone();
+            System.out.println(LINE_SEPARATOR);
+            System.out.println("OK, I've marked this task as not done yet:\n  " + list[index]);
+            System.out.println(LINE_SEPARATOR);
+        }
+    }
+
+    private static int getTaskIndex(String toMark, Task[] list, int counter) {
+        int index = -1;
+        if (Character.isDigit(toMark.charAt(0))) {
+            index = Integer.parseInt(toMark) - 1;
+        } else {
+            for (int i = 0; i < counter; i++) {
+                if (list[i].isTask(toMark)) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+
+    private static void addTask(Task task, Task[] list, int counter) {
+        list[counter] = task;
+        System.out.println(LINE_SEPARATOR);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task.toString());
+        System.out.println("Now you have " + (counter + 1) + " task(s) in the list.");
+        System.out.println(LINE_SEPARATOR);
     }
 
     private static void goodbyes() {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,1 +1,29 @@
-record Pair<T,U>(T t, U u) {}
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean isTask(String task) {
+        return description.equals(task);
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
